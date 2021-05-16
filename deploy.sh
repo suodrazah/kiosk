0a5974fb1e4802133428df6e934dcbfce53f28a0
suo <59600018+suodrazah@users.noreply.github.com>
2021-05-16 20:53:29 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -30,8 +30,8 @@ sudo timedatectl set-timezone Australia/Hobart
 
 #Choose deployment type
 echo "Local or Web kiosk deployment (L/w)?"
-echo "Local - Installs Wordpress and DB, binds Kiosk address to localhost."
-echo "Web - Enter a URL to display in the kiosk."
+echo "Local - Installs NGINX and FileBrowser, or Wordpress and DB. Binds Kiosk address to localhost."
+echo "Web - Requests a URL to display in the kiosk."
 echo "............."
 read TYPE
 TYPE=${TYPE:-L}
@@ -39,55 +39,110 @@ clear
 
 if [ $TYPE = "L" ]; then
 
-    #Set host
-    export URL="http://localhost"
-
-    #Install Docker and Compose
-    curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
-    sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
-
-    #Copy docker-compose.yaml contents
-    rm docker-compose.yaml -f
+    echo "Wordpress or NGINX (W/n)?"
+    echo "Wordpress - Installs Wordpress and DB. Binds Kiosk address to localhost."
+    echo "NGINX - Installs NGINX and Filebrowser. Binds Kiosk to localhost."
+    echo "............."
+    read LOCALTYPE
+    TYPE=${LOCALTYPE:-W}
+    clear
     
-    read -p "Wordpress DB Password?: " DBPWD
-
-    echo "version: '3'" >> docker-compose.yaml
-    echo "services:" >> docker-compose.yaml
-    echo "  wp:" >> docker-compose.yaml
-    echo "    image:  wordpress" >> docker-compose.yaml
-    echo "    volumes:" >> docker-compose.yaml
-    echo "      - wp-data:/var/www/html" >> docker-compose.yaml
-    echo "    networks:" >> docker-compose.yaml
-    echo "      - internal-network" >> docker-compose.yaml
-    echo "    ports:" >> docker-compose.yaml
-    echo "      - 80:80" >> docker-compose.yaml
-    echo "    environment:" >> docker-compose.yaml
-    echo "      - WORDPRESS_DB_HOST=db" >> docker-compose.yaml
-    echo "      - WORDPRESS_DB_USER=db" >> docker-compose.yaml
-    echo "      - WORDPRESS_DB_PASSWORD=$DBPWD" >> docker-compose.yaml
-    echo "      - WORDPRESS_DB_NAME=db" >> docker-compose.yaml
-    echo "  db:" >> docker-compose.yaml
-    echo "    image:  mariadb" >> docker-compose.yaml
-    echo "    volumes:" >> docker-compose.yaml
-    echo "      - db_config:/config" >> docker-compose.yaml
-    echo "      - db:/var/lib/mysql" >> docker-compose.yaml
-    echo "    networks:" >> docker-compose.yaml
-    echo "      - internal-network" >> docker-compose.yaml
-    echo "    environment:" >> docker-compose.yaml
-    echo "      - MYSQL_DATABASE=db" >> docker-compose.yaml
-    echo "      - MYSQL_USER=db" >> docker-compose.yaml
-    echo "      - MYSQL_PASSWORD=$DBPWD" >> docker-compose.yaml
-    echo "      - MYSQL_RANDOM_ROOT_PASSWORD='1'" >> docker-compose.yaml
-    echo "volumes:" >> docker-compose.yaml
-    echo "  wp-data:" >> docker-compose.yaml
-    echo "  db_config:" >> docker-compose.yaml
-    echo "  db:" >> docker-compose.yaml
-    echo "networks:" >> docker-compose.yaml
-    echo "  internal-network:" >> docker-compose.yaml
-
-    #Deploy containers
-    docker-compose up -d
-
+    if [ $LOCALTYPE = "W" ]; then
+    
+        #Set host
+        export URL="http://localhost"
+    
+        #Install Docker and Compose
+        curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
+        sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+    
+        #Copy docker-compose.yaml contents
+        rm docker-compose.yaml -f
+        
+        read -p "Wordpress DB Password?: " DBPWD
+    
+        echo "version: '3'" >> docker-compose.yaml
+        echo "services:" >> docker-compose.yaml
+        echo "  wp:" >> docker-compose.yaml
+        echo "    image:  wordpress" >> docker-compose.yaml
+        echo "    volumes:" >> docker-compose.yaml
+        echo "      - wp-data:/var/www/html" >> docker-compose.yaml
+        echo "    networks:" >> docker-compose.yaml
+        echo "      - internal-network" >> docker-compose.yaml
+        echo "    ports:" >> docker-compose.yaml
+        echo "      - 80:80" >> docker-compose.yaml
+        echo "    environment:" >> docker-compose.yaml
+        echo "      - WORDPRESS_DB_HOST=db" >> docker-compose.yaml
+        echo "      - WORDPRESS_DB_USER=db" >> docker-compose.yaml
+        echo "      - WORDPRESS_DB_PASSWORD=$DBPWD" >> docker-compose.yaml
+        echo "      - WORDPRESS_DB_NAME=db" >> docker-compose.yaml
+        echo "  db:" >> docker-compose.yaml
+        echo "    image:  mariadb" >> docker-compose.yaml
+        echo "    volumes:" >> docker-compose.yaml
+        echo "      - db_config:/config" >> docker-compose.yaml
+        echo "      - db:/var/lib/mysql" >> docker-compose.yaml
+        echo "    networks:" >> docker-compose.yaml
+        echo "      - internal-network" >> docker-compose.yaml
+        echo "    environment:" >> docker-compose.yaml
+        echo "      - MYSQL_DATABASE=db" >> docker-compose.yaml
+        echo "      - MYSQL_USER=db" >> docker-compose.yaml
+        echo "      - MYSQL_PASSWORD=$DBPWD" >> docker-compose.yaml
+        echo "      - MYSQL_RANDOM_ROOT_PASSWORD='1'" >> docker-compose.yaml
+        echo "volumes:" >> docker-compose.yaml
+        echo "  wp-data:" >> docker-compose.yaml
+        echo "  db_config:" >> docker-compose.yaml
+        echo "  db:" >> docker-compose.yaml
+        echo "networks:" >> docker-compose.yaml
+        echo "  internal-network:" >> docker-compose.yaml
+    
+        #Deploy containers
+        docker-compose up -d
+        fi
+    
+     if [ $LOCALTYPE = "n" ]; then
+    
+        #Set host
+        export URL="http://localhost"
+    
+        #Install Docker and Compose
+        curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
+        sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+    
+        #Copy docker-compose.yaml contents
+        rm docker-compose.yaml -f
+        
+        read -p "Wordpress DB Password?: " DBPWD
+    
+        echo "version: '3.3'" >> docker-compose.yaml
+        echo "services:" >> docker-compose.yaml
+        echo "  nginx:" >> docker-compose.yaml
+        echo "    image: nginx" >> docker-compose.yaml
+        echo "    volumes:" >> docker-compose.yaml
+        echo "      - nginx-data:/etc/nginx" >> docker-compose.yaml
+        echo "      - web-data:/usr/share/nginx/html" >> docker-compose.yaml
+        echo "    networks:" >> docker-compose.yaml
+        echo "      - internal-network" >> docker-compose.yaml
+        echo "  filebrowser:" >> docker-compose.yaml
+        echo "    image: hurlenko/filebrowser" >> docker-compose.yaml
+        echo "    restart: always" >> docker-compose.yaml
+        echo "    networks:" >> docker-compose.yaml
+        echo "      - internal-network" >> docker-compose.yaml
+        echo "    environment:" >> docker-compose.yaml
+        echo "      - TZ=Australia/Hobart" >> docker-compose.yaml
+        echo "    volumes:" >> docker-compose.yaml
+        echo "      - filebrowser-data:/config" >> docker-compose.yaml
+        echo "      - web-data:/data" >> docker-compose.yaml
+        echo "networks:" >> docker-compose.yaml
+        echo "  internal-network:" >> docker-compose.yaml
+        echo "volumes:" >> docker-compose.yaml
+        echo "  nginx-data:" >> docker-compose.yaml
+        echo "  filebrowser-data:" >> docker-compose.yaml
+        echo "  web-data:" >> docker-compose.yaml
+    
+        #Deploy containers
+        docker-compose up -d
+        fi   
+    
     fi
 
 
