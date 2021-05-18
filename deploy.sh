96a4169999cf343bff322ae9f7cf04c4cb182a74
suo <59600018+suodrazah@users.noreply.github.com>
2021-05-18 16:37:32 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -121,10 +121,10 @@ if [ $TYPE = "w" ]; then
 fi
 
 #Install minimum GUI components
-sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 
 #Install Chromium
-sudo apt-get install --no-install-recommends chromium-browser -y
+sudo apt install --no-install-recommends chromium-browser -y
 
 #Configure Autostart
 export URL=$URL
